8904b8f896024b74b8262e581b990be5d8e7b1f9
Hijab Zulfiqar <hijabzulfiqar2001@gmail.com>
2023-03-12 16:54:55 +0500

    changes made in html and js file

--- Js/script.js
+++ Js/script.js
@@ -2,27 +2,102 @@
 let screen = document.querySelector(".input-screen");
 let btn = document.querySelectorAll(".btn");
 const btnContainer = document.getElementById("#btn-container");
-// //////////////////////////
-// 
-btn.forEach(function(b) {
-    b.addEventListener('click', function() {
-        if (b.classList.contains("num")) {
-            screen.value += b.innerHTML;
-            console.log(screen.value);
-        }
-        if (b.classList.contains("del")) {
-            screen.value = screen.value.slice(0, screen.value.length-1);
-            console.log(screen.value);
-        }
-        if(b.classList.contains("reset")){
-            screen.value="";
-        }
-        if (b.classList.contains("equal")) {
-            screen.value = eval(screen.value);
-          }
-          if (b.classList.contains("mult")) {
-            screen.value += "*";
-          }
-    })
-})
 
+
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return num1 + num2;
+    }
+    else if (operator === "-") {
+        return num1-num2;
+    }
+    else if (operator === "*") {
+        return num1 * num2;
+    }
+    else if (operator === "/") {
+        return num1 / num2;
+    }
+   
+}
+let num1 = "";
+let num2 = "";
+let operator = "";
+function addToDisplay(num) {
+    document.querySelector(".input-screen").value += num;
+}
+
+function setOperator(operator) {
+    let inputScreen = document.getElementById("input-screen");
+   
+    let currentVal = inputScreen.value;
+    
+    // If the current value on the input screen is not 0 and 
+    // does not already contain an operator, then add the operator
+    if (currentVal !== "0" && !currentVal.includes("+") 
+        && !currentVal.includes("-") && !currentVal.includes("*") 
+        && !currentVal.includes("/")) {
+      inputScreen.value = currentVal + operator;
+    } else if (currentVal.endsWith("+") || currentVal.endsWith("-") 
+    || currentVal.endsWith("*") || currentVal.endsWith("/")) {
+  inputScreen.value = currentVal.slice(0, -1) + operator;
+}
+  }
+
+
+
+function add() {
+    num1 = document.querySelector(".input-screen").value;
+    operator = "+";
+    setOperator(operator);
+}
+function subtract() {
+    num1 = document.querySelector(".input-screen").value;
+    operator = "-";
+    setOperator(operator);
+
+
+    
+}
+function multiply() {
+    num1 = document.querySelector(".input-screen").value;
+    operator = "*";
+    setOperator(operator);
+}
+function divide() {
+    num1 = document.querySelector(".input-screen").value;
+    operator = "/";
+    setOperator(operator);
+}
+function equals() {
+    num2 = document.querySelector(".input-screen").value;
+
+    num2=num2.slice(num2.indexOf(operator)+1,num2.length)
+    console.log(num2)
+    let result = operate(operator, parseInt(num1), parseInt(num2));
+    
+    document.querySelector(".input-screen").value = result;
+    num1 = result;
+   
+    num2 = "";
+    operator = "";
+}
+function clearDisplay() {
+    document.querySelector(".input-screen").value = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+}
+
+function reset() {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    document.querySelector(".input-screen").value = "";
+  }
+    
+      
+  function deleteNumber() {
+    let inputScreen = document.getElementById("input-screen");
+    let currentVal = inputScreen.value;
+    inputScreen.value = currentVal.slice(0, -1);
+}
